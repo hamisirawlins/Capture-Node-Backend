78508998aef78eb7cb7f191b3d02ab7e42bcbc73
Rawlins <rawlins@Rawlinss-MacBook-Air.local>
2022-07-14 16:41:06 +0300

    Working Scaffold

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+//Routes
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://Backend2020:nyuma2020@cluster0.u7bhtdr.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5003;
+
+mongoose.connect(CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))).catch((error) => console.log(error.message));
