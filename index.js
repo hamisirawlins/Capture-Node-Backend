3c51e6f4b672dc12c0bb6e2fcdf8103dcd492125
Rawlins <rawlins@Rawlinss-MacBook-Air.local>
2022-07-17 12:55:38 +0300

    Deployable backend

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import postRoutes from './routes/posts.js'
+import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -14,8 +16,6 @@ app.use(cors());
 //Routes
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://Backend2020:nyuma2020@cluster0.u7bhtdr.mongodb.net/?retryWrites=true&w=majority';
-
 const PORT = process.env.PORT || 5003;
 
-mongoose.connect(CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))).catch((error) => console.log(error.message));
+mongoose.connect(process.env.CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))).catch((error) => console.log(error.message));
