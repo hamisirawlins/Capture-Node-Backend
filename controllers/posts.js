e749de07cdeb61c31be74c1e5aec0fd9c173f915
Rawlins <rawlins@Rawlinss-MacBook-Air.local>
2022-07-17 12:03:06 +0300

    Delete Post

--- controllers/posts.js
+++ controllers/posts.js
@@ -27,3 +27,10 @@ export const updatePost = async (req, res) => {
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
     res.json(updatedPost)
 }
+
+export const deletePost = async (req, res) => {
+    const id = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No Post Available with that Id');
+    await PostMessage.findByIdAndRemove(id);
+    res.json({ message: "Post Successfully deleted" })
+}
